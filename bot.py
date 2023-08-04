526547ccf8439092ae4e094af0efeef7ff3983b4
knownblackhat <knownblackhat@offsec>
2023-08-04 16:39:25 -0400

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,87 @@
+import asyncio
+import time
+from uuid import uuid4
+import os
+from pathlib import Path
+import httpx
+import logging
+import aiofiles
+
+import disnake
+from disnake.ext import commands
+
+from video_segmenter import segment
+
+bot = commands.InteractionBot(intents=disnake.Intents.all())
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(levelname)s: %(message)s",
+    level=logging.INFO,
+    handlers=[logging.StreamHandler()],
+)
+download = set()
+
+async def httpx_download(url:str, file_name: Path, client: httpx.AsyncClient):
+    try:
+        async with client.stream("GET", url) as response:
+            if response.status_code != 200: return
+            async with aiofiles.open(file_name, mode="wb") as file:
+                async for chunk in response.aiter_bytes():
+                    await file.write(chunk)
+        download.add(url)
+    except httpx.NetworkError:
+        logger.exception(f"Error while downloading {url}")
+
+
+async def upload(inter: disnake.GuildCommandInteraction, dir: Path):
+    dir_iter = {x for x in map(lambda x: Path(x), dir.iterdir()) if x.is_file()}
+    to_segment= { file for file in dir_iter if file.stat().st_size/1024**2 > 24}
+    if to_segment:
+        logger.info(f"{len(to_segment)} files found which are more than 25mb detected")
+        for file in to_segment:
+            try:
+                seg_dir=segment(media=file, max_size=24, save_dir=dir)
+            except: continue
+            file.unlink()
+            await upload(inter, seg_dir)
+            seg_dir.rmdir()
+    dir_iter = dir_iter - to_segment
+    total_file = [file for file in map(lambda x: disnake.File(x), dir_iter)]
+    file_grps = [total_file[i: i + 10] for i in range(0, len(total_file), 10)]
+    for file_grp in file_grps:
+        await inter.channel.send(files=file_grp)
+    for file in dir_iter:
+        file.unlink()
+
+@commands.max_concurrency(1, per=commands.BucketType.channel, wait=True)
+@bot.slash_command(name="serve")
+async def serve(inter: disnake.GuildCommandInteraction, attachment: disnake.Attachment):
+    await inter.response.defer(ephemeral=True)
+    await inter.send("Serving...")
+    destination = Path(str(inter.channel.id))
+    try: destination.mkdir()
+    except FileExistsError: ...
+    url_buff = await attachment.read()
+    url_list = url_buff.decode('utf-8').split('\n')
+    url_set = {x for x in url_list}
+    logger.info(f"Downloading {len(url_list)} Items")
+
+    async with httpx.AsyncClient(limits=httpx.Limits(max_connections=10)) as client:
+        tasks = (httpx_download(url=url,
+                                file_name=destination.joinpath(Path(uuid4().__str__() + "." + url.split('.')[-1])),
+                                client=client)
+                 for url in url_list)
+        timer_start = time.perf_counter()
+        await asyncio.gather(*tasks)
+        logger.info(f"{len(url_set)} downloaded within {time.perf_counter() - timer_start}")
+
+    logger.info(f"Not Downloaded {url_set - download}")
+    logger.info(f"Uploading from {destination}")
+    await upload(inter, destination)
+    logger.info("Upload Complete")
+    await inter.channel.send(f"{inter.author.mention} Upload Completed",
+                             allowed_mentions=disnake.AllowedMentions(),
+                             delete_after=5)
+
+bot.run(os.getenv('TOKEN'))
+
---
+++ video_segmenter.py
@@ -0,0 +1,80 @@
+#!/bin/python3
+
+import subprocess
+import sys
+from pathlib import Path
+from typing import Tuple
+import logging
+
+logger = logging.getLogger(__name__)
+
+def get_video_duration_size(video_path: Path) -> Tuple[float, float]:
+    """Returns the duration & size of a video in Minutes & Mb."""
+    command = ['ffprobe', '-v', 'error', '-show_entries',
+               'format=duration', '-of',
+               'default=noprint_wrappers=1:nokey=1', video_path]
+    result = subprocess.run(command, capture_output=True, text=True)
+    duration = float(result.stdout)
+    return (duration,
+            video_path.stat().st_size / (1024 ** 2))
+
+
+def recheck(dir: Path, max_size: int):
+    files = [file for file in dir.iterdir() if file.is_file()]
+    for file in files:
+        file_size = file.stat().st_size / (1024 ** 2)
+        segment_duration = media_stats(file, round(file_size/2)).segment_duration
+        if (file_size >= max_size):
+            trim(media=file,
+                 segment_duration=segment_duration,
+                 out_path=dir, file_name=f"{file.name}%02d.mp4")
+            file.unlink()
+
+
+def media_stats(media: Path, max_size: int):
+    duration, size = get_video_duration_size(media)
+    segment_duration = ((duration / size) * max_size)
+
+    class Meta_data:
+        def __init__(self):
+            self.size = size
+            self.duration = duration
+            self.segment_duration = segment_duration
+    return Meta_data()
+
+
+def trim(media: Path,
+         segment_duration: float,
+         out_path: Path,
+         file_name: str = "%03d.mp4"):
+    command = ['ffmpeg', '-i', media, '-c', 'copy', '-map', '0',
+               '-segment_time', str(segment_duration), '-f',
+               'segment', '-reset_timestamps', '1',
+               f'{out_path.absolute()}/{file_name}']
+    subprocess.run(command, capture_output=True)
+
+
+def segment(media: Path, max_size: int, save_dir: Path) -> Path:
+    out_path = Path(f'{save_dir}/{media.name[:-len(media.suffix)]}')
+    try: out_path.mkdir()
+    except FileExistsError: ...
+    stats = media_stats(media, max_size)
+    if (stats.size <= max_size):
+        raise ValueError(f"Video Size is Already less than {max_size} Mb")
+    elif (stats.segment_duration <= 0):
+        raise ValueError("Max Size Is Too Low")
+    logger.debug(f"[+] Trimming {media.name}")
+    logger.debug(f"[+] Max Size: {max_size} Mb")
+    logger.debug(f"[+] Duration: {stats.duration} Minutes")
+    logger.debug(f"[+] Size: {stats.size} Mb")
+    logger.debug(f"[+] Segment Duration: {stats.segment_duration / 60} Minutes")
+    logger.debug(f"[+] Saving To: {out_path.absolute()}")
+    trim(media, stats.segment_duration, out_path)
+    recheck(out_path, max_size)
+    return out_path
+
+
+if __name__ == "__main__":
+    segment(media=Path(sys.argv[1]),
+        max_size=int(sys.argv[2]),
+        save_dir=Path(sys.argv[3]))
